ad4e10325c55f658b26d47d1af9ac783640ff730
jorvil1993 <jorvil1993@users.noreply.github.com>
2026-09-08 12:02:45 -0400

    Fix: restaurar insignias, iconos y dibujo de Maria en index.html evitando imagenes rotas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="misa-v11";
+const CACHE="misa-v12";
 const ARCHIVOS=["./","./index.html","./cap1.html","./juego.html","./repaso.html","./pausa.html","./gloria.html","./saludo.html","./procesion.html","./canto_entrada.html","./ofrenda.html","./intenciones.html","./tarjeta.html",
   "./cruz.html","./lampara.html","./postura.html","./canciones.html",
   "./manifest.webmanifest","./icon-192.png","./icon-512.png"];
